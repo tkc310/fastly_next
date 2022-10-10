2df1b7dbcae6b676b41dca11c412a83f0eca9e4d
Takashi Sato <takashisato.develop@gmail.com>
2022-10-10 17:37:30 +0900

    add post index page

---
+++ pages/posts/index.tsx
@@ -0,0 +1,42 @@
+import { TPost } from '@/data';
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+const fetchPosts = async (): Promise<TPost[]> => {
+  const res = await fetch('/api/posts');
+  const data = await res.json();
+  return data;
+};
+
+const Posts: NextPage = () => {
+  const [posts, setPosts] = useState<TPost[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const posts = await fetchPosts();
+      setPosts(posts);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <ul>
+        {posts.map((item) => (
+          <li key={item.id}>
+            <h3>
+              <Link href={`/posts/${item.id}`}>{item.title}</Link>
+            </h3>
+            <div>
+              <p>{item.content}</p>
+              <p>{item.id}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Posts;
